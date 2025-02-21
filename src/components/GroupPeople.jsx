774760d070b7ca23070591ba6fcf7e304fa4e28a
Precious Ohanyere <obbyprecious24@gmail.com>
2025-02-21 20:26:34 +0100

    Started with the dashboard design

---
+++ src/components/GroupPeople.jsx
@@ -0,0 +1,7 @@
+export default function GroupPeople(props) {
+    return (
+        <div>
+            <h2>{props.title}</h2>
+        </div>
+    )
+}
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,15 +1,36 @@
 import { useGetCurrentUser } from "../hooks/useDashboard";
 import { Navigate } from "react-router-dom";
+import SideBar from "../components/SideBar";
+import SearchBox from "../components/Search";
+import GroupPeople from "../components/GroupPeople";
 
 export default function Dashboard() {
-  const {data, loading, error} = useGetCurrentUser()
-  if (error) return <Navigate to={"/login"}/>
-  if (loading) return <p>Loading...</p>
+  const { data, loading, error } = useGetCurrentUser();
+  if (error) return <Navigate to={"/login"} />;
+  if (loading) return <p>Loading...</p>;
 
-  localStorage.setItem("user", JSON.stringify(data.getCurrentUser))
+  localStorage.setItem("user", JSON.stringify(data.getCurrentUser));
   return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
+    <main className="bg-[#E5E5E5]">
+      <div className="flex justify-center gap-[10vw]">
+        <div className="flex justify-between gap-8">
+          <div className="mt-4 ml-8">
+            <SideBar />
+          </div>
+
+          <div className="w-[22vw]">
+            <div>
+              <SearchBox />
+            </div>
+
+            <div> <GroupPeople title="Groups"/> </div>
+
+            <div> <GroupPeople title="People"/> </div>
+          </div>
+        </div>
+
+        <div className="border-4 border-red-600"></div>
+      </div>
+    </main>
   );
 }
